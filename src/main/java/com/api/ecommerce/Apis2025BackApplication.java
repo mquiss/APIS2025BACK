3d79544ad5fdf54944871e7226cacaf6e0ec4833
mquiss <mariaquispegamez@gmail.com>
2025-05-22 19:25:49 -0300

    Initial commit

---
+++ src/main/java/com/api/ecommerce/Apis2025BackApplication.java
@@ -0,0 +1,13 @@
+package com.api.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apis2025BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apis2025BackApplication.class, args);
+    }
+
+}
