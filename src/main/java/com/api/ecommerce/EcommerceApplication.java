d9ee3852b8cf110b6bc2560af49dc4d5d17786bb
mquiss <mariaquispegamez@gmail.com>
2025-05-22 19:28:47 -0300

    refactor: rename spring boot application class

--- src/main/java/com/api/ecommerce/EcommerceApplication.java
+++ src/main/java/com/api/ecommerce/EcommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Apis2025BackApplication {
+public class EcommerceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Apis2025BackApplication.class, args);
+        SpringApplication.run(EcommerceApplication.class, args);
     }
 
 }
