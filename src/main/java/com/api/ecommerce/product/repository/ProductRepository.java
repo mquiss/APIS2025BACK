fecc1236a6454eb22497fad9b815bfcbd28c9d18
mquiss <mariaquispegamez@gmail.com>
2025-06-08 18:50:28 -0300

    feat: agregar firma de metodo findByUserId en interfaz de repositorio

--- src/main/java/com/api/ecommerce/product/repository/ProductRepository.java
+++ src/main/java/com/api/ecommerce/product/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.api.ecommerce.product.repository;
 import com.api.ecommerce.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findByUserId(String userId);
 }
