128f83e46febb887da176a2b80235ce9167edb14
mquiss <mariaquispegamez@gmail.com>
2025-06-12 21:15:19 -0300

    agregar anotaciones en order controller

--- src/main/java/com/api/ecommerce/order/controller/OrderController.java
+++ src/main/java/com/api/ecommerce/order/controller/OrderController.java
@@ -1,12 +1,17 @@
 package com.api.ecommerce.order.controller;
 
+import com.api.ecommerce.order.dto.OrderResponse;
 import com.api.ecommerce.order.service.OrderService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
     // TODO: prioridad
     // export const fetchAllOrders = () => api.get('/orders');
@@ -16,6 +21,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @GetMapping("/")
     public ResponseEntity<List<OrderResponse>> getAllOrders() {
         List<OrderResponse> orders = orderService.getOrders();
         return orders.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(orders);
@@ -23,7 +29,8 @@ public class OrderController {
 
     // TODO: opcional
     // obtener compras por userId(mejor que el de arriba)
-    public ResponseEntity<List<OrderResponse>> getOrdersByUserId(String userId) {
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<OrderResponse>> getOrdersByUserId(@PathVariable String userId) {
         List<OrderResponse> orders = orderService.getOrdersByUserId(userId);
         return orders.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(orders);
     }
