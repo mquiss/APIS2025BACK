44b45dc169b97c0e14466818a04e0c54ad971f03
mquiss <mariaquispegamez@gmail.com>
2025-06-14 03:14:19 -0300

    feat(category): crear endpoint para obtener todas las categorias

--- src/main/java/com/api/ecommerce/category/controller/CategoryController.java
+++ src/main/java/com/api/ecommerce/category/controller/CategoryController.java
@@ -1,6 +1,24 @@
 package com.api.ecommerce.category.controller;
 
+import com.api.ecommerce.category.dto.CategoryResponse;
+import com.api.ecommerce.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
 public class CategoryController {
-    // TODO: prioridad
-    // - export const fetchAllCategories = () => api.get('/categories');
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getCategories() {
+        List<CategoryResponse> categories = categoryService.getCategories();
+        return ResponseEntity.ok(categories);
+    }
 }
