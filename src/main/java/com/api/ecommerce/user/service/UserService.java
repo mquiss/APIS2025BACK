247ef0acc3fd31837a9fc80330005231ca942948
zebok <sebiporini@gmail.com>
2025-06-19 00:51:52 -0300

    Uso de Excepciones personalizadas al AuthService y UserService patch

--- src/main/java/com/api/ecommerce/user/service/UserService.java
+++ src/main/java/com/api/ecommerce/user/service/UserService.java
@@ -89,16 +89,25 @@ public class UserService {
 
 
     public UserResponse updatePassword(PasswordRequest request, String id) {
-        User user = userRepository.findById(null == id || id.isEmpty() ? null : new ObjectId(id))
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        System.out.println("ID del usuario: " + id);
-        System.out.println(userMapper.toUserResponse(user));
-        String hashedPassword = passwordEncoder.encode(request.getNewPassword());
-        user.setPassword(hashedPassword);
-        User saved = userRepository.save(user);
+        if (id == null || id.isEmpty()) {
+            throw new IllegalArgumentException("El ID del usuario no puede ser nulo o vacío");
+        }
 
-        UserResponse userResponse = userMapper.toUserResponse(saved);
-        return userResponse; // si usás un mapper
+        try {
+            User user = userRepository.findById(new ObjectId(id))
+                    .orElseThrow(() -> new RecursoNoEncontradoException());
+            String hashedPassword = passwordEncoder.encode(request.getNewPassword());
+            user.setPassword(hashedPassword);
+
+            User saved = userRepository.save(user);
+            return userMapper.toUserResponse(saved);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("ID de usuario inválido: " + e.getMessage());
+        } catch (RecursoNoEncontradoException e) {
+            throw new RecursoNoEncontradoException();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al actualizar la contraseña: " + e.getMessage());
+        }
     }
 
     public Optional<User> updateUser(String id, User userDetails) {
