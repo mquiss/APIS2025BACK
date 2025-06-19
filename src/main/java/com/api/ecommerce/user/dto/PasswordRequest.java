da90fe4cf09f11679ce42afe2c3514a970a36ca2
mquiss <mariaquispegamez@gmail.com>
2025-06-19 20:15:05 -0300

    agregar anotacion data de lombok y validaciones de jakarta

--- src/main/java/com/api/ecommerce/user/dto/PasswordRequest.java
+++ src/main/java/com/api/ecommerce/user/dto/PasswordRequest.java
@@ -1,30 +1,12 @@
 package com.api.ecommerce.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class PasswordRequest {
+    @NotBlank(message = "Old password is required")
     private String oldPassword;
+    @NotBlank(message = "New password is required")
     private String newPassword;
-
-    public PasswordRequest() {
-    }
-
-    public PasswordRequest(String oldPassword, String newPassword) {
-        this.oldPassword = oldPassword;
-        this.newPassword = newPassword;
-    }
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
 }
