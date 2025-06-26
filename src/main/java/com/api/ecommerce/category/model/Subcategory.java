6fc0f3ece95f4f46e924ad85a160ac660ae6c9c4
mquiss <mariaquispegamez@gmail.com>
2025-06-26 20:11:05 -0300

    fix(category): especificar field para que no devuelva null

--- src/main/java/com/api/ecommerce/category/model/Subcategory.java
+++ src/main/java/com/api/ecommerce/category/model/Subcategory.java
@@ -1,9 +1,11 @@
 package com.api.ecommerce.category.model;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 public class Subcategory {
+    @Field("id")
     private String id;
     private String name;
 }
