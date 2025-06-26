2ce92ad3a3b4a64f22007b631d99e25b9234bb5a
mquiss <mariaquispegamez@gmail.com>
2025-06-26 20:12:37 -0300

    feat(category): crear metodo para obtener subcategoria por id

--- src/main/java/com/api/ecommerce/category/service/CategoryService.java
+++ src/main/java/com/api/ecommerce/category/service/CategoryService.java
@@ -3,19 +3,23 @@ package com.api.ecommerce.category.service;
 import com.api.ecommerce.category.dto.CategoryResponse;
 import com.api.ecommerce.category.mapper.CategoryMapper;
 import com.api.ecommerce.category.model.Category;
+import com.api.ecommerce.category.model.Subcategory;
 import com.api.ecommerce.category.repository.CategoryRepository;
 import com.api.ecommerce.common.exception.RecursoNoEncontradoException;
+import com.api.ecommerce.common.util.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
+    private final Mapper mapper;
 
     public List<CategoryResponse> getCategories() {
         return categoryRepository
@@ -29,5 +33,10 @@ public class CategoryService {
         return categoryRepository
                 .findById(id)
                 .orElseThrow(RecursoNoEncontradoException::new);
+    }
+
+    public Subcategory findSubcategoryById(String id, String subId) {
+        Category category = categoryRepository.findById(mapper.mapStringToObjectId(id)).orElseThrow(RecursoNoEncontradoException::new);
+        return category.getSubcategories().stream().filter(sub -> Objects.equals(sub.getId(), subId)).findFirst().orElseThrow(RecursoNoEncontradoException::new);
     }
 }
