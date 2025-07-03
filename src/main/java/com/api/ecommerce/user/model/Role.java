4a8890bdc8abd098f08ebe86babb06ea3d7d872c
mquiss <mariaquispegamez@gmail.com>
2025-07-02 23:44:28 -0300

    implementar metodos de userdetails y agregar atributo role

---
+++ src/main/java/com/api/ecommerce/user/model/Role.java
@@ -0,0 +1,5 @@
+package com.api.ecommerce.user.model;
+
+public enum Role {
+    USER
+}
--- src/main/java/com/api/ecommerce/user/model/User.java
+++ src/main/java/com/api/ecommerce/user/model/User.java
@@ -2,34 +2,73 @@ package com.api.ecommerce.user.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
 @Document(collection = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     private ObjectId id;
 
     @Indexed(unique = true)
     private String email;
-    private String username; // FIXME: por alguna razon no acepta usernames duplicados en db
+    @Field("username")
+    private String alias;
     private String password;
     private String firstName;
     private String lastName;
     private Address address;
     private String avatar;
+    private Role role;
 
     @CreatedDate
     private Instant createdAt;
 
     @LastModifiedDate
     private Instant updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + (role != null ? role.name() : "USER")));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
