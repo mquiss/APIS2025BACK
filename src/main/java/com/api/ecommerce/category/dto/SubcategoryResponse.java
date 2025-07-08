2095480260de797f9b80a07a891dd6adcd6cad18
mquiss <mariaquispegamez@gmail.com>
2025-07-08 19:21:32 -0300

    crear subcategory response e implementar en mapper

--- src/main/java/com/api/ecommerce/category/dto/SubcategoryResponse.java
+++ src/main/java/com/api/ecommerce/category/dto/SubcategoryResponse.java
@@ -3,12 +3,9 @@ package com.api.ecommerce.category.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @Builder
-public class CategoryResponse {
+public class SubcategoryResponse {
     private String id;
     private String name;
-    private List<SubcategoryResponse> subcategories;
 }
--- src/main/java/com/api/ecommerce/category/mapper/CategoryMapper.java
+++ src/main/java/com/api/ecommerce/category/mapper/CategoryMapper.java
@@ -1,8 +1,11 @@
 package com.api.ecommerce.category.mapper;
 
 import com.api.ecommerce.category.dto.CategoryResponse;
+import com.api.ecommerce.category.dto.SubcategoryResponse;
 import com.api.ecommerce.category.model.Category;
+import com.api.ecommerce.category.model.Subcategory;
 
 public interface CategoryMapper {
     CategoryResponse toCategoryResponse(Category category);
+    SubcategoryResponse toSubcategoryResponse(Subcategory subcategory);
 }
